a5bd15a1d180b75acab76417fcea6fb0d82b5e26
Navneet Mishra <138274399+Satyanand-Navneet-Mishra@users.noreply.github.com>
2023-11-04 16:37:41 +0530

    Update fibonacci.cpp

--- C++ Programs/fibonacci.cpp
+++ C++ Programs/fibonacci.cpp
@@ -1,23 +1,28 @@
 #include <iostream>
 using namespace std;
-/*fibonnaci == 0,1,1,2,3,5,8,13....n*/
 
 void fibonacci(int num){
-    int var1=0, var2=1, nexTerm;
-    cout<<var1<<" "<<var2<<" ";
-
-    for(int i=2;i<num;i++){
-        nexTerm = (var1+var2);
-        cout<<nexTerm<<" ";
-        var1=var2;
-        var2=nexTerm;
+    int ele1=0, ele2=1;
+    cout<<"Fibonacci Sequence: "<<endl;
+    cout<<ele1<<" "<<ele2<<" ";
+    if(num<=2){
+        return;
+    }else{
+        for(int i=0;i<=num;i++){
+            int elenext=(ele1+ele2);
+            cout<<elenext<<" ";
+            ele1=ele2;
+            ele2=elenext;
+        }
     }
-};
+}
 
 int main(){
     int n;
+    cout<<"Number Of Elements: ";
     cin>>n;
 
     fibonacci(n);
+
     return 0;
 }
