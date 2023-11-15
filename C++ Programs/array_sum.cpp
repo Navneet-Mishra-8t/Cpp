c0cb15b349fc4b92d6e805313a9d0a31af2b0d8d
Navneet Mishra <138274399+Satyanand-Navneet-Mishra@users.noreply.github.com>
2023-11-15 10:31:18 +0530

    Update array_sum.cpp
    By Using Dynamic Allocation

--- C++ Programs/array_sum.cpp
+++ C++ Programs/array_sum.cpp
@@ -1,26 +1,30 @@
-#include <iostream>
+#include<iostream>
+
 using namespace std;
 
-int array_sum(int array[], int size){
-    int sum=0;
-    for(int i=0;i<size;i++){
-        sum += array[i];
-    }
-    return sum;
+int array_sum(int arr[], int n){
+
+	int sum = 0;
+	for(int i = 0; i < n; i++){
+		sum += arr[i];
+	}
+
+	return sum;
 }
 
+
 int main(){
-    int n;
-    cout<<"Size Of Array: ";
-    cin>>n;
-    int arr[n];
+	int n;
+	cout << "Size Of Array: ";
+	cin >> n;
 
-    cout<<"Elements: ";
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
-    }
+	int *arr = new int[n];
 
-    cout<<"Sum Of Array's All Element: "<<array_sum(arr,n);
+	for(int i = 0; i < n; i++){
+		cin >> arr[i];
+	}
 
-    return 0;
+	cout << "Sum Of All Elements Of The Array: " << array_sum(arr, n);
+
+	return 0;
 }
