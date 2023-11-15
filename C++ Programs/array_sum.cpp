4f673f5725a37092958773caf9e1d123bb1bc700
Navneet Mishra <138274399+Satyanand-Navneet-Mishra@users.noreply.github.com>
2023-11-15 12:01:28 +0530

    Update array_sum.cpp
    By Using Dynamic Memory Allocation.
    This code prevents memory leaks and dangling pointers.

--- C++ Programs/array_sum.cpp
+++ C++ Programs/array_sum.cpp
@@ -2,13 +2,6 @@
 
 using namespace std;
 
-/*
-Dynamic Memory Allocation:
-
-Allocate memory for an array of integers using new and initialize it with user input.
-Allocate memory for a string dynamically and concatenate it with another string.
-*/
-
 int array_sum(int arr[], int n){
 	int sum = 0;
 	for(int i = 0; i < n; i++){
