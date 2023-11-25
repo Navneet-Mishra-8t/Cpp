3bc2e38864a150e58a51f1364ce65a32d38639cc
Navneet Mishra <138274399+Satyanand-Navneet-Mishra@users.noreply.github.com>
2023-11-25 17:09:41 +0530

    Add files via upload

---
+++ C++ Programs/ASCII_val.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+                        
+using namespace std;
+ 
+int main(){
+    //variables
+    char ch;
+
+    //taking the character from user
+    cout << "Enter A Character: ";
+    cin >> ch;
+
+    //printing the ASCII value 
+    cout << "The ASCII value of " << ch << " is " << int(ch) << endl;                            
+                      
+    return 0;
+}
+ 
