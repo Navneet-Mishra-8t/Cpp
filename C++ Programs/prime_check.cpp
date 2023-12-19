f3d44280000243f0ab65aa5f4592a1d46b19dba0
Navneet Mishra <138274399+Satyanand-Navneet-Mishra@users.noreply.github.com>
2023-12-19 16:49:40 +0530

    Update prime_check.cpp

--- C++ Programs/prime_check.cpp
+++ C++ Programs/prime_check.cpp
@@ -1,20 +1,38 @@
-#include <bits/stdc++.h>
+#include <iostream>
+                        
 using namespace std;
 
-int main(){
-    int n;
-    cin>>n;
-    
-    int i;
-    for(i = 2; i < n; i++){
-        if(n%i == 0){
-            cout<<"Non Prime"<<endl;
+//function for checking the prime number
+bool isPrime(int n){
+    int i = 2;
+
+    //loop
+    for (i; i < n; i++){
+        if (n % i == 0){
             break;
         }
     }
-    if(i==n){
-        cout<<"Prime";
+
+    if (i == n){
+        return true;
+    }else {
+        return false;
     }
 
+}
+ 
+int main(){
+    //taking the number from the user
+    int num;
+    cout << "Number: ";
+    cin >> num;
+
+    //generating output
+    if (isPrime(num)){
+        cout << "The Number " << num << " Is Prime!";
+    }else {
+        cout << "The Number " << num << " Isn't Prime!";
+    }
+                      
     return 0;
 }
