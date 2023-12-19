c886fa6caac7867e239f7f0991b0bf7b98b94eca
Navneet Mishra <138274399+Satyanand-Navneet-Mishra@users.noreply.github.com>
2023-12-19 16:36:15 +0530

    Update fibonacci.cpp

--- C++ Programs/fibonacci.cpp
+++ C++ Programs/fibonacci.cpp
@@ -1,28 +1,38 @@
 #include <iostream>
+                        
 using namespace std;
 
-void fibonacci(int num){
-    int ele1=0, ele2=1;
-    cout<<"Fibonacci Sequence: "<<endl;
-    cout<<ele1<<" "<<ele2<<" ";
-    if(num<=2){
+//function to generate fibonacci sequence till the range given
+void fibonacci(int range){
+    int f_ele = 0, s_ele = 1;
+
+    //printing the elements (loop)
+    cout << "Fibonacci Sequence: ";
+    cout << f_ele << " " << s_ele << " ";
+    if (range<=2){
         return;
-    }else{
-        for(int i=0;i<=num;i++){
-            int elenext=(ele1+ele2);
-            cout<<elenext<<" ";
-            ele1=ele2;
-            ele2=elenext;
+    }
+    else {
+        for (int i = 0; i < range; i++){
+        int n_ele = (f_ele + s_ele);
+        cout << n_ele << " ";
+
+        f_ele = s_ele;
+        s_ele = n_ele;
         }
     }
+
 }
-
+ 
 int main(){
-    int n;
-    cout<<"Number Of Elements: ";
-    cin>>n;
+    //taking the range of the fibonacci sequence
+    int range;
+    cout << "Range Of The Fibonacci Sequence: ";
+    cin >> range;
 
-    fibonacci(n);
-
+    //function to print the fibonacci sequence
+    fibonacci(range);                                
+                      
     return 0;
 }
+ 
