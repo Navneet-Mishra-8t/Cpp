8860b888e566da5663e2438ea4f51d1f186ced09
Navneet Mishra <138274399+Satyanand-Navneet-Mishra@users.noreply.github.com>
2023-12-20 16:33:24 +0530

    Update reverse_num.cpp

--- C++ Programs/reverse_num.cpp
+++ C++ Programs/reverse_num.cpp
@@ -1,18 +1,30 @@
 #include <iostream>
+                        
 using namespace std;
+ 
+//function to reverse the number 
+void reverse_num(int n){
+    int r_num = 0;
+
+    while (n != 0){
+        int l_num = (n % 10);
+        r_num = (r_num * 10) + l_num;
+
+        n /= 10;
+    }
+
+    cout << "Reversed Number: " << r_num;
+}
 
 int main(){
-    int n;//1234
-    cin>>n;
-    int rnum=0;
+    //taking number from user
+    int n;
+    cout << "Number: ";
+    cin >> n;
 
-    do{
-        int digit=n%10;
-        rnum = (rnum * 10) + digit;
-        n /= 10;//123.4
-    } while(n!=0);
-
-    cout<<rnum<<endl;
-
+    //calling function to print the result
+    reverse_num(n);                        
+                      
     return 0;
 }
+ 
